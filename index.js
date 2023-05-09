0108f27877b4ba9e7ab5b2fe2e38b69e40d62239
GabrielRocha1031 <54642896+GabrielRocha1031@users.noreply.github.com>
2023-05-09 15:15:32 -0300

    Add files via upload
    Final

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const canvas = document.getElementById('canvas');
 const gl = canvas.getContext('webgl2');
 
-const vertexShaderSource = document.getElementById('vertexShader').text;
-const fragmentShaderSource = document.getElementById('fragmentShader').text;
+// Get shader sources from HTML script tags
+const vertexShaderSource = document.getElementById('vertexShader').textContent.trim();
+const fragmentShaderSource = document.getElementById('fragmentShader').textContent.trim();
 
-const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-gl.shaderSource(vertexShader, vertexShaderSource);
-gl.compileShader(vertexShader);
+// Create shaders, compile and attach to a program
+const createShader = (gl, type, source) => {
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    console.error(`An error occurred while compiling the shader: ${gl.getShaderInfoLog(shader)}`);
+    gl.deleteShader(shader);
+    return null;
+  }
+  return shader;
+};
 
-const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-gl.shaderSource(fragmentShader, fragmentShaderSource);
-gl.compileShader(fragmentShader);
-
-const devicePixelRatio = window.devicePixelRatio || 1;
-canvas.width = canvas.clientWidth * devicePixelRatio;
-canvas.height = canvas.clientHeight * devicePixelRatio;
-gl.viewport(0, 0, canvas.width, canvas.height);
+const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
 
 const program = gl.createProgram();
 gl.attachShader(program, vertexShader);
@@ -23,16 +27,24 @@ gl.attachShader(program, fragmentShader);
 gl.linkProgram(program);
 gl.useProgram(program);
 
+// Set canvas resolution based on device pixel ratio
+const devicePixelRatio = window.devicePixelRatio || 1;
+canvas.width = canvas.clientWidth * devicePixelRatio;
+canvas.height = canvas.clientHeight * devicePixelRatio;
+gl.viewport(0, 0, canvas.width, canvas.height);
+
+// Define vertices and bind them to a buffer
+const positions = [
+  -1.0, -1.0, 0.0,
+  1.0, -1.0, 0.0,
+  -1.0, 1.0, 0.0,
+  1.0, 1.0, 0.0,
+];
 const positionBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-const positions = [
-    -1.0, -1.0, 0.0,
-    1.0, -1.0, 0.0,
-    -1.0, 1.0, 0.0,
-    1.0, 1.0, 0.0,
-];
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
+// Get attribute and uniform locations
 const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
 gl.enableVertexAttribArray(positionAttributeLocation);
 gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
@@ -40,42 +52,28 @@ gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
 const resolutionUniformLocation = gl.getUniformLocation(program, 'u_resolution');
 gl.uniform2f(resolutionUniformLocation, canvas.width, canvas.height);
 
-const sphereColor = gl.getUniformLocation(program, 'u_sphereColor');
+const inkColor = gl.getUniformLocation(program, 'u_inkColor');
 const cubeColor = gl.getUniformLocation(program, 'u_cubeColor');
-const obstacleColor = gl.getUniformLocation(program, 'u_obstacleColor');
 
-gl.uniform3f(sphereColor, 0.1, 0.1, 0.7); // azul
-gl.uniform3f(cubeColor, 1.0, 1.0, 1.0); // branco
-gl.uniform3f(obstacleColor, 0.1, 0.1, 0.7); // azul
-
-function fract(x) {
-    return x - Math.floor(x);
-}
-const xObstacle = gl.getUniformLocation(program, 'u_xObstacle')
+gl.uniform3f(inkColor, 0.9, 0.0, 0.0); // blue
+gl.uniform3f(cubeColor, 0.8, 0.8, 1.0); // white
 
 const timeUniformLocation = gl.getUniformLocation(program, 'u_time');
-
 const u_y = gl.getUniformLocation(program, 'u_y');
-gl.uniform1f(u_y, 0.0);
-
 const u_x = gl.getUniformLocation(program, 'u_x');
-gl.uniform1f(u_x, 0.0);
-
 const u_ygota2 = gl.getUniformLocation(program, 'u_ygota2');
-gl.uniform1f(u_ygota2, 0.0);
-
 const u_zCam = gl.getUniformLocation(program, 'u_zCam');
 
-var time = 0
-var y = 3.0
-var ygota2 = -1.7
-var x= 1.0
-var zCam = 5.0
-var ydir = 'up'
+// valores iniciais
+let y = 3.0;
+let ygota2 = -1.7;
+let x = 1.0;
+let zCam = 5.5;
+let ydir = 'up';
 
-function render() {
-    y -= 0.04
-    x += 0.04
+const updateUniforms = () => {
+  y -= 0.04
+  x += 0.04
     
     if(ydir=='up'){
         ygota2 += 0.02
@@ -104,7 +102,8 @@ function render() {
     gl.uniform1f(u_zCam, zCam);
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
-    requestAnimationFrame(render);
+    requestAnimationFrame(updateUniforms);
 }
 
-requestAnimationFrame(render);
+requestAnimationFrame(updateUniforms);
+
